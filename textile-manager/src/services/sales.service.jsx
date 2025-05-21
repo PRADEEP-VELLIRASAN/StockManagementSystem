5b1f44adeee28b198a399a7ab3d7a4779583e915
PRADEEP V <146934039+PRADEEP-VELLIRASAN@users.noreply.github.com>
2025-05-21 21:49:54 +0530

    Update sales.service.jsx

--- textile-manager/src/services/sales.service.jsx
+++ textile-manager/src/services/sales.service.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import authHeader from './authHeader';
 
-const API_URL = 'http://localhost:5000/api/sales/';
+const API_URL = 'https://stockmanagementsystem-d5kp.onrender.com/api/sales/';
 
 const getAllSales = () => {
   return axios.get(API_URL, { headers: authHeader() });
