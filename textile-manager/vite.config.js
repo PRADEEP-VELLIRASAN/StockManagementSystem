9b99b131e4563e5ea278982532c512cc959e18b2
PRADEEP V <146934039+PRADEEP-VELLIRASAN@users.noreply.github.com>
2025-05-21 22:41:07 +0530

    Update vite.config.js

--- textile-manager/vite.config.js
+++ textile-manager/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:5000', // Your backend server
+        target: 'https://stockmanagementsystem-d5kp.onrender.com', // Your backend server
         changeOrigin: true,
         secure: false,
       },
